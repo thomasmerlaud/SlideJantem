12ce0aa180606bd29aa40621c2c467f2600e4b88
Thomas MERLAUD <thomas.merlaud@student.junia.com>
2022-06-07 15:12:04 +0200

    up end

---
+++ code/jantemcode/typing3.js
@@ -0,0 +1,47 @@
+// METTRE LE TEXTE A LA PLACE EN DESSOUS
+// QUAND YA UNE VIRGULE CA ATTEND QUELQUES SECONDES (2sec) je peux changer ca aussi
+var aText = new Array(
+    "Congratulations, you've finished the game !",
+    "You did not all that for nothing ! By painting all these maps, you painted something...",
+    "Look at this !"
+  
+
+);
+
+
+
+
+var iSpeed = 45; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+
+function typewriter() {
+    sContents = ' ';
+    iRow = Math.max(0, iIndex - iScrollAt);
+    var destination = document.getElementById("typedtext");
+
+    while (iRow < iIndex) {
+        sContents += aText[iRow++] + '<br />';
+    }
+
+    var cursor = "<span class='cursor'>|</span>"
+    destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + cursor;
+    if (iTextPos++ == iArrLength) {
+        iTextPos = 0;
+        iIndex++;
+        if (iIndex != aText.length) {
+            iArrLength = aText[iIndex].length;
+            setTimeout("typewriter()", 1250);
+        }
+    } else {
+        setTimeout("typewriter()", iSpeed);
+    }
+}
+
+
+typewriter();
