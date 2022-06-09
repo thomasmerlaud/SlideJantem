d222d6aba7926d32a503f76dbf06a0163429bedc
MiloMontuori <milo.montuori@student.junia.com>
2022-06-09 15:54:10 +0200

    up NO DB

--- bdd/connectDBexte.php
+++ bdd/connectDBexte.php
@@ -1,7 +1,7 @@
 <?php
     $hostname="remotemysql.com:3306"; //nom du serveur (localhost)
     $username="d6xINafhrw";//nom d'utilisateur pour accéder au serveur (root)
-    $password="xZFwOcD5Oo"; //mot de passe pour accéder au serveur (root)
+    $password="eIrGNoQDXP"; //mot de passe pour accéder au serveur (root)
     $dbname="d6xINafhrw"; //nom de la base de données
     
     $connexion = mysqli_connect($hostname, $username, $password, $dbname);
